a7b6bb48448824fbefb2d19946199e0ace95bcac
Christoph Kappestein <k42b3.x@gmail.com>
2016-10-29 14:37:44 +0200

    fix test folder according to namespace

--- tests/Service/Log/QueryFilterTest.php
+++ tests/Service/Log/QueryFilterTest.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Impl\Tests\Table\Log;
+namespace Fusio\Impl\Tests\Service\Log;
 
 use Fusio\Impl\Service\Log\QueryFilter;
 
