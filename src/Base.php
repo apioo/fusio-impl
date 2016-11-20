1dc73c52041b576a76a8000c4bad3c00481e7ed2
Christoph Kappestein <k42b3.x@gmail.com>
2016-11-20 20:06:49 +0100

    increase version

--- src/Base.php
+++ src/Base.php
@@ -30,7 +30,7 @@ namespace Fusio\Impl;
  */
 class Base
 {
-    const VERSION = '0.5.0';
+    const VERSION = '0.5.1';
 
     public static function getVersion()
     {
--- src/Database/Installer.php
+++ src/Database/Installer.php
@@ -152,6 +152,7 @@ class Installer
     public static function getUpgradePath()
     {
         return [
+            '0.5.1',
             '0.5.0',
             '0.4.1',
             '0.4.0',
