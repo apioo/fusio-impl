f524ebc2cbd5639a03f779b73b68d2c4448358c2
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-04-02 11:10:08 +0200

    remove auto increment since the sql must work on sqlite and mysql

--- tests/Console/System/resource/v1_schema.sql
+++ tests/Console/System/resource/v1_schema.sql
@@ -1,7 +1,7 @@
 
 -- acme_foo
 CREATE TABLE acme_foo (
-  id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY,
   title char(64) NOT NULL,
   content text NOT NULL,
   insertDate datetime NOT NULL
@@ -9,7 +9,7 @@ CREATE TABLE acme_foo (
 
 -- acme_bar
 CREATE TABLE acme_bar (
-  id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY,
   title char(64) NOT NULL,
   content text NOT NULL,
   insertDate datetime NOT NULL
