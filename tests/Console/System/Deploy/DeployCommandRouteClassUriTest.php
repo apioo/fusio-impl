169942cab7f578dfbab9401243d38f65a2a32661
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-08-25 10:14:07 +0200

    add deploy action uri tests

---
+++ tests/Console/System/Deploy/DeployCommandRouteClassUriTest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Console\System\Deploy;
+
+use Fusio\Engine\Factory\Resolver\PhpClass;
+use Fusio\Impl\Controller\SchemaApiController;
+use Fusio\Impl\Tests\Fixture;
+use PSX\Api\Resource;
+use PSX\Framework\Test\ControllerDbTestCase;
+use PSX\Framework\Test\Environment;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * DeployCommandRouteClassUriTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class DeployCommandRouteClassUriTest extends ControllerDbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testCommandRoutesActionClass()
+    {
+        $command = Environment::getService('console')->find('system:deploy');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'file'    => __DIR__ . '/../resource/deploy_routes_action_class_uri.yaml',
+        ]);
+
+        $display = $commandTester->getDisplay();
+
+        $this->assertRegExp('/- \[CREATED\] action Fusio_Impl_Tests_Console_System_Action_TestAction/', $display, $display);
+        $this->assertRegExp('/- \[CREATED\] routes \/bar/', $display, $display);
+
+        // check action
+        $action = $this->connection->fetchAssoc('SELECT id, class, engine, config FROM fusio_action WHERE name = :name', [
+            'name' => 'Fusio_Impl_Tests_Console_System_Action_TestAction',
+        ]);
+
+        $this->assertEquals(4, $action['id']);
+        $this->assertContains('Fusio\Impl\Tests\Console\System\Action\TestAction', $action['class']);
+        $this->assertEquals(PhpClass::class, $action['engine']);
+        $this->assertEquals(null, $action['config']);
+
+        // check routes
+        $route = $this->connection->fetchAssoc('SELECT id, status, methods, controller FROM fusio_routes WHERE path = :path', [
+            'path' => '/bar',
+        ]);
+
+        $this->assertEquals(Fixture::getLastRouteId() + 2, $route['id']);
+        $this->assertEquals(1, $route['status']);
+        $this->assertEquals('GET|POST|PUT|DELETE', $route['methods']);
+        $this->assertEquals(SchemaApiController::class, $route['controller']);
+
+        // check methods
+        $methods = $this->connection->fetchAll('SELECT id, routeId, method, version, status, active, public, parameters, request, action FROM fusio_routes_method WHERE routeId = :routeId', [
+            'routeId' => $route['id'],
+        ]);
+
+        $this->assertEquals(1, count($methods));
+        $this->assertEquals(Fixture::getLastRouteId() + 2, $methods[0]['routeId']);
+        $this->assertEquals('GET', $methods[0]['method']);
+        $this->assertEquals(1, $methods[0]['version']);
+        $this->assertEquals(Resource::STATUS_DEVELOPMENT, $methods[0]['status']);
+        $this->assertEquals(1, $methods[0]['active']);
+        $this->assertEquals(1, $methods[0]['public']);
+        $this->assertEquals(null, $methods[0]['parameters']);
+        $this->assertEquals(null, $methods[0]['request']);
+        $this->assertEquals(4, $methods[0]['action']);
+
+        // check responses
+        $responses = $this->connection->fetchAll('SELECT methodId, code, response FROM fusio_routes_response WHERE methodId = :methodId', [
+            'methodId' => $methods[0]['id'],
+        ]);
+
+        $this->assertEquals(1, count($responses));
+        $this->assertEquals(200, $responses[0]['code']);
+        $this->assertEquals(1, $responses[0]['response']);
+    }
+}
