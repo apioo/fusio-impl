fcc99d5ea5b9e45ca90867131e5bc439be4334f5
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-10-05 12:13:18 +0200

    fix namespace

--- tests/Console/Cronjob/ExecuteCommandTest.php
+++ tests/Console/Cronjob/ExecuteCommandTest.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Impl\Tests\Console\User;
+namespace Fusio\Impl\Tests\Console\Cronjob;
 
 use Fusio\Impl\Tests\Fixture;
 use PSX\Framework\Test\ControllerDbTestCase;
