740ebca95ac13e454603f30425bb3c8b948ae0e6
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-11-03 00:46:49 +0100

    add welcome test

---
+++ tests/Controller/WelcomeTest.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Controller;
+
+use Fusio\Impl\Base;
+use Fusio\Impl\Tests\Fixture;
+use PSX\Framework\Test\ControllerDbTestCase;
+
+/**
+ * WelcomeTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class WelcomeTest extends ControllerDbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testGet()
+    {
+        $response = $this->sendRequest('/', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b41344388feed85bc362e518387fdc8c81b896bfe5e794131e1469770571d873'
+        ));
+
+        $version = Base::VERSION;
+        $body    = (string) $response->getBody();
+        $expect  = <<<JSON
+{
+    "message": "Congratulations the installation of Fusio was successful",
+    "version": "{$version}",
+    "links": [
+        {
+            "rel": "about",
+            "name": "http:\/\/fusio-project.org"
+        }
+    ]
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testPost()
+    {
+        $response = $this->sendRequest('/', 'POST', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b41344388feed85bc362e518387fdc8c81b896bfe5e794131e1469770571d873'
+        ));
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testPut()
+    {
+        $response = $this->sendRequest('/', 'PUT', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b41344388feed85bc362e518387fdc8c81b896bfe5e794131e1469770571d873'
+        ));
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testPatch()
+    {
+        $response = $this->sendRequest('/', 'PATCH', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b41344388feed85bc362e518387fdc8c81b896bfe5e794131e1469770571d873'
+        ));
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testDelete()
+    {
+        $response = $this->sendRequest('/', 'DELETE', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer b41344388feed85bc362e518387fdc8c81b896bfe5e794131e1469770571d873'
+        ));
+
+        $this->assertEquals(405, $response->getStatusCode());
+    }
+
+}
