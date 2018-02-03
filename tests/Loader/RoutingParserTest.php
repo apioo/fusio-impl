c683114ed5954708cad6835ee72b4ff9fdfd7713
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-02-03 19:33:59 +0100

    add test whetehr we can resolve all routes

---
+++ tests/Loader/RoutingParserTest.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Loader;
+
+use Fusio\Impl\Controller\SchemaApiController;
+use Fusio\Impl\Database\Installer;
+use Fusio\Impl\Loader\RoutingParser;
+use Fusio\Impl\Tests\DbTestCase;
+use PSX\Framework\Loader\Context;
+use PSX\Http\Request;
+use PSX\Http\RequestInterface;
+use PSX\Uri\Uri;
+
+/**
+ * RoutingParserTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class RoutingParserTest extends DbTestCase
+{
+    /**
+     * @dataProvider resolveProvider
+     */
+    public function testResolve($method, $path, $controller)
+    {
+        $request = new Request(new Uri($path), $method);
+        $context = new Context();
+
+        $parser  = new RoutingParser($this->connection);
+        $request = $parser->resolve($request, $context);
+
+        $this->assertInstanceOf(RequestInterface::class, $request);
+        $this->assertEquals($controller, $context->get(Context::KEY_SOURCE));
+    }
+
+    public function resolveProvider()
+    {
+        $data = [
+            ['GET', '/', SchemaApiController::class],
+            ['GET', '/foo', SchemaApiController::class],
+        ];
+
+        $version = Installer::getLatestVersion();
+        $inserts = $version->getInstallInserts();
+        $routes  = $inserts['fusio_routes'] ?? [];
+
+        foreach ($routes as $route) {
+            $path = $route['path'];
+            $path = preg_replace('/\$[a-z_]+<\[0\-9\]\+>/', '1', $path);
+
+            $data[] = ['GET', $path, $route['controller']];
+        }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider resolveFailProvider
+     */
+    public function testResolveFailProvider($method, $path)
+    {
+        $request = new Request(new Uri($path), $method);
+        $context = new Context();
+
+        $parser  = new RoutingParser($this->connection);
+        $request = $parser->resolve($request, $context);
+
+        $this->assertEmpty($request);
+    }
+
+    public function resolveFailProvider()
+    {
+        return [
+            ['GET', '/bar'],
+            ['GET', '/baz'],
+        ];
+    }
+}
