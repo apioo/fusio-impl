f9e651f7ab93b7964f08bc26e2eff85b6bf647f9
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-04-23 22:51:40 +0200

    update version class name

--- src/Database/Version/Version305.php
+++ src/Database/Version/Version305.php
@@ -41,13 +41,13 @@ use PSX\Framework\Controller\Generator;
 use PSX\Framework\Controller\Tool;
 
 /**
- * Version304
+ * Version305
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    http://fusio-project.org
  */
-class Version304 implements VersionInterface
+class Version305 implements VersionInterface
 {
     public function getSchema()
     {
