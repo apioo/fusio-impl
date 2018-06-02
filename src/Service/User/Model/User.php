ca77914ed313cc56a91213bce41a224d6868ff74
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-06-02 21:54:43 +0200

    fix php doc

--- src/Service/User/Model/User.php
+++ src/Service/User/Model/User.php
@@ -22,7 +22,7 @@
 namespace Fusio\Impl\Service\User\Model;
 
 /**
- * User model which contains all informations from the remote user
+ * User model which contains all information from the remote user
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
  * @license http://www.gnu.org/licenses/agpl-3.0
