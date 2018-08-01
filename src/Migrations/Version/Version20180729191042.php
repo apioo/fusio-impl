0a2f35a665de2de59c3d7b0fa8f3c8961ea827e5
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-08-01 20:45:56 +0200

    fix column rename migration

--- src/Migrations/Version/Version20180729191042.php
+++ src/Migrations/Version/Version20180729191042.php
@@ -20,70 +20,70 @@ class Version20180729191042 extends AbstractMigration
         $this->skipIf($appTable->hasColumn('user_id'), 'Column names already migrated');
         $this->skipIf($this->platform->getName() !== 'mysql', 'Can rename column only on mysql');
 
-        $this->addSql('ALTER TABLE fusio_app RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_app RENAME COLUMN appKey TO app_key');
-        $this->addSql('ALTER TABLE fusio_app RENAME COLUMN appSecret TO app_secret');
-        
-        $this->addSql('ALTER TABLE fusio_app_scope RENAME COLUMN appId TO app_id');
-        $this->addSql('ALTER TABLE fusio_app_scope RENAME COLUMN scopeId TO scope_id');
-        
-        $this->addSql('ALTER TABLE fusio_app_token RENAME COLUMN appId TO app_id');
-        $this->addSql('ALTER TABLE fusio_app_token RENAME COLUMN userId TO user_id');
-        
-        $this->addSql('ALTER TABLE fusio_app_code RENAME COLUMN appId TO app_id');
-        $this->addSql('ALTER TABLE fusio_app_code RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_app_code RENAME COLUMN redirectUri TO redirect_uri');
-        
-        $this->addSql('ALTER TABLE fusio_audit RENAME COLUMN appId TO app_id');
-        $this->addSql('ALTER TABLE fusio_audit RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_audit RENAME COLUMN refId TO ref_id');
-        
-        $this->addSql('ALTER TABLE fusio_cronjob RENAME COLUMN executeDate TO execute_date');
-        $this->addSql('ALTER TABLE fusio_cronjob RENAME COLUMN exitCode TO exit_code');
-        
-        $this->addSql('ALTER TABLE fusio_cronjob_error RENAME COLUMN cronjobId TO cronjob_id');
-        
-        $this->addSql('ALTER TABLE fusio_event_response RENAME COLUMN triggerId TO trigger_id');
-        $this->addSql('ALTER TABLE fusio_event_response RENAME COLUMN subscriptionId TO subscription_id');
-        $this->addSql('ALTER TABLE fusio_event_response RENAME COLUMN executeDate TO execute_date');
-        $this->addSql('ALTER TABLE fusio_event_response RENAME COLUMN insertDate TO insert_date');
-        
-        $this->addSql('ALTER TABLE fusio_event_subscription RENAME COLUMN eventId TO event_id');
-        $this->addSql('ALTER TABLE fusio_event_subscription RENAME COLUMN userId TO user_id');
-        
-        $this->addSql('ALTER TABLE fusio_event_trigger RENAME COLUMN eventId TO event_id');
-        $this->addSql('ALTER TABLE fusio_event_trigger RENAME COLUMN insertDate TO insert_date');
-        
-        $this->addSql('ALTER TABLE fusio_log RENAME COLUMN routeId TO route_id');
-        $this->addSql('ALTER TABLE fusio_log RENAME COLUMN appId TO app_id');
-        $this->addSql('ALTER TABLE fusio_log RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_log RENAME COLUMN userAgent TO user_agent');
-        $this->addSql('ALTER TABLE fusio_log RENAME COLUMN executionTime TO execution_time');
-        
-        $this->addSql('ALTER TABLE fusio_log_error RENAME COLUMN logId TO log_id');
-        
-        $this->addSql('ALTER TABLE fusio_routes_method RENAME COLUMN routeId TO route_id');
-        $this->addSql('ALTER TABLE fusio_routes_method RENAME COLUMN schemaCache TO schema_cache');
-        $this->addSql('ALTER TABLE fusio_routes_method RENAME COLUMN actionCache TO action_cache');
-        
-        $this->addSql('ALTER TABLE fusio_routes_response RENAME COLUMN methodId TO method_id');
-        
-        $this->addSql('ALTER TABLE fusio_rate RENAME COLUMN rateLimit TO rate_limit');
-        
-        $this->addSql('ALTER TABLE fusio_rate_allocation RENAME COLUMN rateId TO rate_id');
-        $this->addSql('ALTER TABLE fusio_rate_allocation RENAME COLUMN routeId TO route_id');
-        $this->addSql('ALTER TABLE fusio_rate_allocation RENAME COLUMN appId TO app_id');
-        
-        $this->addSql('ALTER TABLE fusio_user RENAME COLUMN remoteId TO remote_id');
-        
-        $this->addSql('ALTER TABLE fusio_scope_routes RENAME COLUMN scopeId TO scope_id');
-        $this->addSql('ALTER TABLE fusio_scope_routes RENAME COLUMN routeId TO route_id');
-        
-        $this->addSql('ALTER TABLE fusio_user_grant RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_user_grant RENAME COLUMN appId TO app_id');
-        
-        $this->addSql('ALTER TABLE fusio_user_scope RENAME COLUMN userId TO user_id');
-        $this->addSql('ALTER TABLE fusio_user_scope RENAME COLUMN scopeId TO scope_id');
+        $this->addSql('ALTER TABLE fusio_app CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_app CHANGE appKey app_key varchar(255)');
+        $this->addSql('ALTER TABLE fusio_app CHANGE appSecret app_secret varchar(255)');
+
+        $this->addSql('ALTER TABLE fusio_app_scope CHANGE appId app_id int(11)');
+        $this->addSql('ALTER TABLE fusio_app_scope CHANGE scopeId scope_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_app_token CHANGE appId app_id int(11)');
+        $this->addSql('ALTER TABLE fusio_app_token CHANGE userId user_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_app_code CHANGE appId app_id int(11)');
+        $this->addSql('ALTER TABLE fusio_app_code CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_app_code CHANGE redirectUri redirect_uri varchar(255)');
+
+        $this->addSql('ALTER TABLE fusio_audit CHANGE appId app_id int(11)');
+        $this->addSql('ALTER TABLE fusio_audit CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_audit CHANGE refId ref_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_cronjob CHANGE executeDate execute_date datetime');
+        $this->addSql('ALTER TABLE fusio_cronjob CHANGE exitCode exit_code int(11)');
+
+        $this->addSql('ALTER TABLE fusio_cronjob_error CHANGE cronjobId cronjob_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_event_response CHANGE triggerId trigger_id int(11)');
+        $this->addSql('ALTER TABLE fusio_event_response CHANGE subscriptionId subscription_id int(11)');
+        $this->addSql('ALTER TABLE fusio_event_response CHANGE executeDate execute_date int(11)');
+        $this->addSql('ALTER TABLE fusio_event_response CHANGE insertDate insert_date int(11)');
+
+        $this->addSql('ALTER TABLE fusio_event_subscription CHANGE eventId event_id int(11)');
+        $this->addSql('ALTER TABLE fusio_event_subscription CHANGE userId user_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_event_trigger CHANGE eventId event_id int(11)');
+        $this->addSql('ALTER TABLE fusio_event_trigger CHANGE insertDate insert_date datetime');
+
+        $this->addSql('ALTER TABLE fusio_log CHANGE routeId route_id int(11)');
+        $this->addSql('ALTER TABLE fusio_log CHANGE appId app_id int(11)');
+        $this->addSql('ALTER TABLE fusio_log CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_log CHANGE userAgent user_agent varchar(255)');
+        $this->addSql('ALTER TABLE fusio_log CHANGE executionTime execution_time int(11)');
+
+        $this->addSql('ALTER TABLE fusio_log_error CHANGE logId log_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_routes_method CHANGE routeId route_id int(11)');
+        $this->addSql('ALTER TABLE fusio_routes_method CHANGE schemaCache schema_cache longtext');
+        $this->addSql('ALTER TABLE fusio_routes_method CHANGE actionCache action_cache longtext');
+
+        $this->addSql('ALTER TABLE fusio_routes_response CHANGE methodId method_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_rate CHANGE rateLimit rate_limit int(11)');
+
+        $this->addSql('ALTER TABLE fusio_rate_allocation CHANGE rateId rate_id int(11)');
+        $this->addSql('ALTER TABLE fusio_rate_allocation CHANGE routeId route_id int(11)');
+        $this->addSql('ALTER TABLE fusio_rate_allocation CHANGE appId app_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_user CHANGE remoteId remote_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_scope_routes CHANGE scopeId scope_id int(11)');
+        $this->addSql('ALTER TABLE fusio_scope_routes CHANGE routeId route_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_user_grant CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_user_grant CHANGE appId app_id int(11)');
+
+        $this->addSql('ALTER TABLE fusio_user_scope CHANGE userId user_id int(11)');
+        $this->addSql('ALTER TABLE fusio_user_scope CHANGE scopeId scope_id int(11)');
     }
 
     /**
