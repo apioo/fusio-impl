213c12117148e3a3e8935e1a4fd6d2b71894242c
Christoph Kappestein <christoph.kappestein@gmail.com>
2018-12-06 22:09:23 +0100

    add migration test case

---
+++ tests/Migrations/MigrationTest.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Migration;
+
+use Fusio\Impl\Backend;
+use Fusio\Impl\Tests\Fixture;
+use PSX\Framework\Test\DbTestCase;
+use PSX\Framework\Test\Environment;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * This test runs all migrations up and down
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class MigrationTest extends DbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testMigrate()
+    {
+        $app = Environment::getService('console');
+
+        // keep current schema which we restore at the end
+        $schema = $this->connection->getSchemaManager()->createSchema();
+
+        // drop all tables
+        $tables = $this->connection->getSchemaManager()->listTableNames();
+        foreach ($tables as $tableName) {
+            $this->connection->executeQuery('DROP TABLE ' . $tableName);
+        }
+
+        // migrate all versions up
+        $versions = $this->getVersions();
+        $command  = $app->find('migration:execute');
+
+        foreach ($versions as $version) {
+            $commandTester = new CommandTester($command);
+            $commandTester->execute([
+                'command'      => $command->getName(),
+                'version'      => $version,
+                '--up'         => null,
+            ], [
+                'interactive'  => false
+            ]);
+        }
+
+        // migrate all versions down
+        $versions = array_reverse($versions);
+        foreach ($versions as $version) {
+            $commandTester = new CommandTester($command);
+            $commandTester->execute([
+                'command'      => $command->getName(),
+                'version'      => $version,
+                '--down'       => null,
+            ], [
+                'interactive'  => false
+            ]);
+        }
+
+        // now we should have only the migration table
+        $tables = $this->connection->getSchemaManager()->listTableNames();
+
+        $this->assertEquals(['fusio_migration_versions'], $tables);
+
+        // revert schema
+        $fromSchema = $this->connection->getSchemaManager()->createSchema();
+        $queries    = $fromSchema->getMigrateToSql($schema, $this->connection->getDatabasePlatform());
+
+        foreach ($queries as $query) {
+            $this->connection->executeQuery($query);
+        }
+    }
+
+    private function getVersions()
+    {
+        $result = [];
+        $path   = __DIR__ . '/../../src/Migrations/Version';
+        $files  = scandir($path);
+
+        foreach ($files as $file) {
+            if ($file[0] != '.') {
+                $result[] = substr(pathinfo($file, PATHINFO_FILENAME), 7);
+            }
+        }
+
+        return $result;
+    }
+}
