861186198f1c6beb0e5bd66ee10fb0d78a09fd6c
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-04-29 22:06:07 +0200

    update schema and view for backend plan

---
+++ src/Backend/View/Plan/Contract.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Backend\View\Plan;
+
+use Fusio\Impl\Table;
+use PSX\Sql\Condition;
+use PSX\Sql\Sql;
+use PSX\Sql\ViewAbstract;
+
+/**
+ * Contract
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class Contract extends ViewAbstract
+{
+    public function getCollection($startIndex = null, $count = null, $search = null)
+    {
+        if (empty($startIndex) || $startIndex < 0) {
+            $startIndex = 0;
+        }
+
+        if (empty($count) || $count < 1 || $count > 1024) {
+            $count = 16;
+        }
+
+        $condition = new Condition();
+        $condition->equals('status', Table\Plan::STATUS_ACTIVE);
+
+        if (!empty($search)) {
+            $condition->like('name', '%' . $search . '%');
+        }
+
+        $definition = [
+            'totalResults' => $this->getTable(Table\Plan\Contract::class)->getCount($condition),
+            'startIndex' => $startIndex,
+            'itemsPerPage' => $count,
+            'entry' => $this->doCollection([$this->getTable(Table\Plan\Contract::class), 'getAll'], [$startIndex, $count, 'id', Sql::SORT_DESC, $condition], [
+                'id' => $this->fieldInteger('id'),
+                'userId' => $this->fieldInteger('user_id'),
+                'planId' => $this->fieldInteger('plan_id'),
+                'status' => $this->fieldInteger('status'),
+                'amount' => $this->fieldNumber('amount'),
+                'points' => $this->fieldInteger('points'),
+                'insertDate' => $this->fieldDateTime('insert_date'),
+            ]),
+        ];
+
+        return $this->build($definition);
+    }
+
+    public function getEntity($id)
+    {
+        $definition = $this->doEntity([$this->getTable(Table\Plan\Contract::class), 'get'], [$id], [
+            'id' => $this->fieldInteger('id'),
+            'userId' => $this->fieldInteger('user_id'),
+            'planId' => $this->fieldInteger('plan_id'),
+            'status' => $this->fieldInteger('status'),
+            'amount' => $this->fieldNumber('amount'),
+            'points' => $this->fieldInteger('points'),
+            'insertDate' => $this->fieldDateTime('insert_date'),
+
+        ]);
+
+        return $this->build($definition);
+    }
+}
