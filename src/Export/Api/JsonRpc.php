1a3ef1a38eaf1f7ead6e4509927cec49fe527583
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-07-13 21:28:03 +0200

    use custom evaluator

--- src/Export/Api/JsonRpc.php
+++ src/Export/Api/JsonRpc.php
@@ -21,27 +21,17 @@
 
 namespace Fusio\Impl\Export\Api;
 
-use Datto\JsonRpc\Evaluator;
-use Datto\JsonRpc\Exceptions\MethodException;
 use Datto\JsonRpc\Server;
-use Fusio\Engine\Context as EngineContext;
-use Fusio\Engine\Repository;
-use Fusio\Engine\Request;
 use Fusio\Impl\Authorization\Authorization;
 use Fusio\Impl\Export;
 use Fusio\Impl\Export\Schema;
-use Fusio\Impl\Filter\Authentication;
-use Fusio\Impl\Filter\Logger;
-use Fusio\Impl\Filter\RequestLimit;
-use Fusio\Impl\Rpc\RpcContext;
+use Fusio\Impl\Rpc\Evaluator;
 use Fusio\Impl\Table\Routes\Method;
 use PSX\Api\DocumentedInterface;
 use PSX\Api\Resource;
 use PSX\Framework\Controller\SchemaApiAbstract;
 use PSX\Http\Environment\HttpContextInterface;
-use PSX\Http\Exception as StatusCode;
 use PSX\Http\Filter\UserAgentEnforcer;
-use PSX\Record\Record;
 
 /**
  * JsonRpc
@@ -50,7 +40,7 @@ use PSX\Record\Record;
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    http://fusio-project.org
  */
-class JsonRpc extends SchemaApiAbstract implements DocumentedInterface, Evaluator
+class JsonRpc extends SchemaApiAbstract implements DocumentedInterface
 {
     /**
      * @Inject
@@ -95,21 +85,21 @@ class JsonRpc extends SchemaApiAbstract implements DocumentedInterface, Evaluato
 
     /**
      * @Inject
+     * @var \Fusio\Impl\Service\Log
+     */
+    protected $logService;
+
+    /**
+     * @Inject
+     * @var \Fusio\Impl\Service\Security\TokenValidator
+     */
+    protected $securityTokenValidator;
+
+    /**
+     * @Inject
      * @var \Fusio\Impl\Service\Plan\Payer
      */
     protected $planPayerService;
-
-    /**
-     * @Inject
-     * @var \Fusio\Engine\Repository\AppInterface
-     */
-    protected $appRepository;
-
-    /**
-     * @Inject
-     * @var \Fusio\Engine\Repository\UserInterface
-     */
-    protected $userRepository;
 
     /**
      * @return array
@@ -121,26 +111,6 @@ class JsonRpc extends SchemaApiAbstract implements DocumentedInterface, Evaluato
         // it is required for every request to have an user agent which
         // identifies the client
         $filter[] = new UserAgentEnforcer();
-
-        $filter[] = new Authentication(
-            $this->connection,
-            $this->context,
-            $this->config->get('fusio_project_key'),
-            $this->appRepository,
-            $this->userRepository,
-            false
-        );
-
-        $filter[] = new RequestLimit(
-            $this->rateService,
-            $this->appRepository,
-            $this->context
-        );
-
-        $filter[] = new Logger(
-            $this->connection,
-            $this->context
-        );
 
         return $filter;
     }
@@ -166,71 +136,19 @@ class JsonRpc extends SchemaApiAbstract implements DocumentedInterface, Evaluato
      */
     protected function doPost($record, HttpContextInterface $context)
     {
-        $server = new Server($this);
-        $reply  = $server->reply(json_encode($record));
+        $server = new Server(new Evaluator(
+            $this->processor,
+            $this->securityTokenValidator,
+            $this->rateService,
+            $this->logService,
+            $this->planPayerService,
+            $this->tableManager->getTable(Method::class),
+            $this->config,
+            $this->request
+        ));
+
+        $reply = $server->reply(json_encode($record));
 
         return \json_decode($reply);
-    }
-
-    public function evaluate($method, $arguments)
-    {
-        $operationId = $method;
-        $record      = Record::from($arguments);
-
-        /** @var Method $methodTable */
-        $methodTable = $this->tableManager->getTable(Method::class);
-        $method      = $methodTable->getMethodByOperationId($operationId);
-
-        if (empty($method)) {
-            throw new MethodException();
-        }
-
-        $baseUrl  = $this->config->get('psx_url') . '/' . $this->config->get('psx_dispatch');
-        $context  = new EngineContext($this->context->getRouteId(), $baseUrl, $this->context->getApp(), $this->context->getUser());
-
-        $rpcContext = new RpcContext(
-            $method['method'],
-            $arguments
-        );
-
-        $request  = new Request($rpcContext, $record);
-        $response = null;
-        $actionId = $method['action'];
-        $costs    = (int) $method['costs'];
-        $cache    = $method['action_cache'];
-
-        if ($costs > 0) {
-            // as anonymous user it is not possible to pay
-            if ($this->context->getUser()->isAnonymous()) {
-                throw new StatusCode\ForbiddenException('This action costs points because of this you must be authenticated in order to call this action');
-            }
-
-            // in case the method has assigned costs check whether the user has
-            // enough points
-            $remaining = $this->context->getUser()->getPoints() - $costs;
-            if ($remaining < 0) {
-                throw new StatusCode\ClientErrorException('Your account has not enough points to call this action. Please purchase new points in order to execute this action', 429);
-            }
-
-            $this->planPayerService->pay($costs, $context);
-        }
-
-        if ($actionId > 0) {
-            if ($method['status'] != Resource::STATUS_DEVELOPMENT && !empty($cache)) {
-                // if the method is not in dev mode we load the action from the
-                // cache
-                $this->processor->push(Repository\ActionMemory::fromJson($cache));
-
-                $response = $this->processor->execute($actionId, $request, $context);
-
-                $this->processor->pop();
-            } else {
-                $response = $this->processor->execute($actionId, $request, $context);
-            }
-        } else {
-            throw new StatusCode\ServiceUnavailableException('No action provided');
-        }
-
-        return $response->getBody();
     }
 }
