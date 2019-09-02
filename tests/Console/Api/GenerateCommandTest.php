86d8f250172a51e3c5f08f10c8bed6ff3e708772
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-09-02 16:20:24 +0200

    add api generate command test

---
+++ tests/Console/Api/GenerateCommandTest.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2018 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Console\Api;
+
+use Fusio\Impl\Tests\Fixture;
+use PSX\Framework\Test\ControllerDbTestCase;
+use PSX\Framework\Test\Environment;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * GenerateCommandTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class GenerateCommandTest extends ControllerDbTestCase
+{
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testCommand()
+    {
+        $command = Environment::getService('console')->find('api:generate');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+            '-i'      => 'internal',
+            '-f'      => 'client.php',
+            'dir'     => __DIR__ . '/output',
+        ]);
+
+        $actual = $commandTester->getDisplay();
+
+        $this->assertEquals('Successful!', trim($actual));
+
+        // check files
+        $files = scandir(__DIR__ . '/output');
+
+        $this->assertTrue(count($files) > 40); // simply check whether files are available the exact count is variable
+    }
+}
