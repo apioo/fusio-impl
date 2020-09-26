255dd3bc7d5cdb1bf9e5fd34b102d3dc33259918
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-09-26 23:26:10 +0200

    renamed transformer

--- src/Deploy/Transformer/Route.php
+++ src/Deploy/Transformer/Route.php
@@ -27,13 +27,13 @@ use Fusio\Impl\Service\System\SystemAbstract;
 use RuntimeException;
 
 /**
- * Routes
+ * Route
  *
  * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
  * @license http://www.gnu.org/licenses/agpl-3.0
  * @link    http://fusio-project.org
  */
-class Routes extends TransformerAbstract
+class Route extends TransformerAbstract
 {
     public function transform(array $data, \stdClass $import, $basePath)
     {
