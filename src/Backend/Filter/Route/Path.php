221882ba066d816273a74be9be79a31128f3ed6c
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-09-26 23:27:09 +0200

    moved path filter

--- src/Backend/Filter/Route/Path.php
+++ src/Backend/Filter/Route/Path.php
@@ -19,7 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Fusio\Impl\Backend\Filter\Routes;
+namespace Fusio\Impl\Backend\Filter\Route;
 
 use PSX\Validate\FilterAbstract;
 
