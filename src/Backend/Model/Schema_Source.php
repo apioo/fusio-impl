8454d3f0355a41be4acc4638368455fc36709d23
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-11-05 22:48:19 +0100

    update model

---
+++ src/Backend/Model/Schema_Source.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Fusio\Impl\Backend\Model;
+
+/**
+ * @extends \PSX\Record\Record<mixed>
+ * @Description("Contains the TypeSchema specification")
+ */
+class Schema_Source extends \PSX\Record\Record
+{
+}
