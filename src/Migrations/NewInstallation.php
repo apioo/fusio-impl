624349bbf21a71c6d8a50c3437c2b0deed98bc70
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-11-07 19:17:00 +0100

    use correct status code

--- src/Migrations/NewInstallation.php
+++ src/Migrations/NewInstallation.php
@@ -258,7 +258,7 @@ class NewInstallation
                 ],
                 '/marketplace' => [
                     'GET' => new Method(Backend\Action\Marketplace\GetAll::class, null, [200 => Backend\Model\Marketplace_Collection::class], null, 'backend.marketplace'),
-                    'POST' => new Method(Backend\Action\Marketplace\Install::class, Backend\Model\Marketplace_Install::class, [200 => Backend\Model\Marketplace_Install::class], null, 'backend.marketplace'),
+                    'POST' => new Method(Backend\Action\Marketplace\Install::class, Backend\Model\Marketplace_Install::class, [201 => Backend\Model\Marketplace_Install::class], null, 'backend.marketplace'),
                 ],
                 '/marketplace/:app_name' => [
                     'GET' => new Method(Backend\Action\Marketplace\Get::class, null, [200 => Backend\Model\Marketplace_Local_App::class], null, 'backend.marketplace'),
