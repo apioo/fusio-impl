0c1e786d1c946961c561f3ff0309f1dc8372e48a
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-29 00:02:16 +0100

    remove get default scopes and set role

--- src/Service/User/Register.php
+++ src/Service/User/Register.php
@@ -88,15 +88,12 @@ class Register
             $status = Table\User::STATUS_ACTIVE;
         }
 
-        $scopes = $this->userService->getDefaultScopes();
-
         $user = new User_Create();
         $user->setRoleId((int) $this->configService->getValue('role_default'));
         $user->setStatus($status);
         $user->setName($register->getName());
         $user->setEmail($register->getEmail());
         $user->setPassword($register->getPassword());
-        $user->setScopes($scopes);
 
         $userId = $this->userService->create($user, UserContext::newAnonymousContext());
 
