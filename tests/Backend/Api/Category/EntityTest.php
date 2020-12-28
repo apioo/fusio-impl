6b284119ff9ecd9620e5d9c7681375e7132d7f3b
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-28 20:58:44 +0100

    add category test cases

---
+++ tests/Backend/Api/Category/EntityTest.php
@@ -0,0 +1,183 @@
+<?php
+/*
+ * Fusio
+ * A web-application to create dynamically RESTful APIs
+ *
+ * Copyright (C) 2015-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fusio\Impl\Tests\Backend\Api\Category;
+
+use Fusio\Impl\Tests\Documentation;
+use Fusio\Impl\Tests\Fixture;
+use PSX\Framework\Test\ControllerDbTestCase;
+use PSX\Framework\Test\Environment;
+
+/**
+ * EntityTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.gnu.org/licenses/agpl-3.0
+ * @link    http://fusio-project.org
+ */
+class EntityTest extends ControllerDbTestCase
+{
+    private $id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->id = Fixture::getId('fusio_category', 'backend');
+    }
+
+    public function getDataSet()
+    {
+        return Fixture::getDataSet();
+    }
+
+    public function testDocumentation()
+    {
+        $response = $this->sendRequest('/system/doc/*/backend/category/' . $this->id, 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $actual = Documentation::getResource($response);
+        $expect = file_get_contents(__DIR__ . '/resource/entity.json');
+
+        $this->assertJsonStringEqualsJsonString($expect, $actual, $actual);
+    }
+
+    public function testGet()
+    {
+        $response = $this->sendRequest('/backend/category/' . $this->id, 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<JSON
+{
+    "id": 2,
+    "status": 1,
+    "name": "backend"
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testGetNotFound()
+    {
+        Environment::getContainer()->get('config')->set('psx_debug', false);
+
+        $response = $this->sendRequest('/backend/category/10', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": false,
+    "title": "Internal Server Error",
+    "message": "Could not find category"
+}
+JSON;
+
+        $this->assertEquals(404, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testPost()
+    {
+        $response = $this->sendRequest('/backend/category/' . $this->id, 'POST', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(405, $response->getStatusCode(), $body);
+    }
+
+    public function testPut()
+    {
+        $response = $this->sendRequest('/backend/category/' . $this->id, 'PUT', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ), json_encode([
+            'name' => 'foo',
+        ]));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": true,
+    "message": "Category successful updated"
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+
+        // check database
+        $sql = Environment::getService('connection')->createQueryBuilder()
+            ->select('id', 'status', 'name')
+            ->from('fusio_category')
+            ->where('id = :id')
+            ->getSQL();
+
+        $row = Environment::getService('connection')->fetchAssoc($sql, ['id' => $this->id]);
+
+        $this->assertEquals(1, $row['status']);
+        $this->assertEquals('foo', $row['name']);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->sendRequest('/backend/category/' . $this->id, 'DELETE', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": true,
+    "message": "Category successful deleted"
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+
+        // check database
+        $sql = Environment::getService('connection')->createQueryBuilder()
+            ->select('id', 'status')
+            ->from('fusio_category')
+            ->where('id = :id')
+            ->getSQL();
+
+        $row = Environment::getService('connection')->fetchAssoc($sql, ['id' => $this->id]);
+
+        $this->assertEquals(0, $row['status']);
+    }
+}
