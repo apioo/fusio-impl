d06916fe76149baad1fefc401831204cbc094a6d
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-12-29 15:49:00 +0100

    update search

--- tests/Backend/Api/Role/CollectionTest.php
+++ tests/Backend/Api/Role/CollectionTest.php
@@ -89,7 +89,7 @@ JSON;
 
     public function testGetSearch()
     {
-        $response = $this->sendRequest('/backend/role?search=back', 'GET', array(
+        $response = $this->sendRequest('/backend/role?search=ack', 'GET', array(
             'User-Agent'    => 'Fusio TestCase',
             'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
         ));
