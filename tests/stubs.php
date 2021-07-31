256c1766b92d9811a6a7264a3255e4f53c2e63ef
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-07-31 23:34:21 +0200

    change thrift package

--- tests/stubs.php
+++ tests/stubs.php
@@ -20,34 +20,3 @@ namespace PhpAmqpLib\Message
         public const DELIVERY_MODE_PERSISTENT = 0;
     }
 }
-
-namespace Thrift\Transport
-{
-    class TSocket
-    {
-        public function __construct(string $host, int $port)
-        {
-        }
-    }
-
-    class TBufferedTransport
-    {
-        public function __construct($transport, int $rBufSize = 512, int $wBufSize = 512)
-        {
-        }
-    }
-}
-
-namespace Thrift\Protocol
-{
-    class TBinaryProtocol
-    {
-        public function __construct($transport, bool $strictRead = false, bool $strictWrite = true)
-        {
-        }
-
-        public function open()
-        {
-        }
-    }
-}
