f873dbda863f2d7cf1b608c4a780f69b930d7c12
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-03-12 22:22:22 +0100

    fix config update

--- src/Migrations/Version/Version20220226222841.php
+++ src/Migrations/Version/Version20220226222841.php
@@ -21,7 +21,7 @@ final class Version20220226222841 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         $configs = [
-            [Table\Config::FORM_STRING, 'mail_points_subject', 'Subject of the points threshold mail', 'Fusio points threshold reached', 'Subject of the points threshold mail'],
+            [Table\Config::FORM_STRING, 'mail_points_subject', 'Subject of the points threshold mail', 'Fusio points threshold reached'],
             [Table\Config::FORM_TEXT, 'mail_points_body', 'Body of the points threshold mail', 'Hello {name},' . "\n\n" . 'your account has reached the configured threshold of {points} points.' . "\n" . 'If your account reaches 0 points your are not longer able to invoke specific endpoints.' . "\n" . 'To prevent this please go to the developer portal to purchase new points:' . "\n" . '{apps_url}/developer'],
             [Table\Config::FORM_NUMBER, 'points_threshold', 'If a user goes below this points threshold we send an information to the user', 0],
         ];
