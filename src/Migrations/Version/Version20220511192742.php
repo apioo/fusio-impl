e65f7e6461eeeafa6ec1b48255c55c6e4139ebc6
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-05-14 11:51:33 +0200

    add new config settings

--- src/Migrations/Version/Version20220511192742.php
+++ src/Migrations/Version/Version20220511192742.php
@@ -6,6 +6,7 @@ namespace Fusio\Impl\Migrations\Version;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Fusio\Impl\Table;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -44,6 +45,19 @@ final class Version20220511192742 extends AbstractMigration
         if (!$userTable->hasColumn('plan_id')) {
             $userTable->addColumn('plan_id', 'integer', ['notnull' => false]);
             $userTable->addColumn('external_id', 'string', ['notnull' => false]);
+        }
+
+        // add new config
+        $configs = [
+            [Table\Config::FORM_STRING, 'payment_stripe_secret', 'The stripe webhook secret which is needed to verify a webhook request', ''],
+            [Table\Config::FORM_STRING, 'payment_currency', 'The three-character ISO-4217 currency code which is used to process payments', ''],
+        ];
+
+        foreach ($configs as $row) {
+            $id = $this->connection->fetchOne('SELECT id FROM fusio_config WHERE name = ?', [$row[1]]);
+            if (empty($id)) {
+                $this->addSql('INSERT INTO fusio_config (type, name, description, value) VALUES (?, ?, ?, ?)', $row);
+            }
         }
     }
 
