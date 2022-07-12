a9aea91ad995e4bc3396ce80b66af1868fc23161
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-12 22:13:39 +0200

    case int to string

--- src/Backend/Action/Trash/Restore.php
+++ src/Backend/Action/Trash/Restore.php
@@ -58,7 +58,7 @@ class Restore extends ActionAbstract
 
         $this->restorer->restore(
             $request->get('type'),
-            $id
+            (string) $id
         );
 
         return [
