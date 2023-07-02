25f0d1349f6f4fe91a72221c74373d0d8c54739f
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-07-02 10:11:36 +0200

    remove getConnectionParams

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,38 +4,6 @@ require(__DIR__ . '/../vendor/autoload.php');
 
 $container = require_once __DIR__ . '/../container.php';
 
-define('FUSIO_IN_TEST', true);
-
 /** @var \PSX\Framework\Test\Environment $environment */
 $environment = $container->get(\PSX\Framework\Test\Environment::class);
-$environment->setup(getConnectionParams());
-
-function getConnectionParams(): array
-{
-    switch (getenv('DB')) {
-        case 'mysql':
-            return [
-                'dbname'   => 'fusio',
-                'user'     => 'root',
-                'password' => 'test1234',
-                'host'     => 'localhost',
-                'driver'   => 'pdo_mysql',
-            ];
-
-        case 'postgres':
-            return [
-                'dbname'   => 'fusio',
-                'user'     => 'postgres',
-                'password' => 'postgres',
-                'host'     => 'localhost',
-                'driver'   => 'pdo_pgsql',
-            ];
-
-        default:
-        case 'sqlite':
-            return [
-                'memory' => true,
-                'driver' => 'pdo_sqlite',
-            ];
-    }
-}
+$environment->setup();
