7795476e9ec10b3d023543413343ed0f00609658
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-04-21 12:59:25 +0200

    add new adapters which dont have an extension requirements

--- tests/Command/System/RegisterCommandTest.php
+++ tests/Command/System/RegisterCommandTest.php
@@ -64,12 +64,15 @@ class RegisterCommandTest extends ControllerDbTestCase
 
         $actual = include $file;
         $expect = [
+            \Fusio\Adapter\Amqp\Adapter::class,
+            \Fusio\Adapter\Beanstalk\Adapter::class,
             \Fusio\Adapter\Cli\Adapter::class,
             \Fusio\Adapter\Fcgi\Adapter::class,
             \Fusio\Adapter\File\Adapter::class,
             \Fusio\Adapter\GraphQL\Adapter::class,
             \Fusio\Adapter\Http\Adapter::class,
             \Fusio\Adapter\Php\Adapter::class,
+            \Fusio\Adapter\Redis\Adapter::class,
             \Fusio\Adapter\Smtp\Adapter::class,
             \Fusio\Adapter\Soap\Adapter::class,
             \Fusio\Adapter\Sql\Adapter::class,
@@ -101,12 +104,15 @@ class RegisterCommandTest extends ControllerDbTestCase
 
         $actual = include $file;
         $expect = [
+            \Fusio\Adapter\Amqp\Adapter::class,
+            \Fusio\Adapter\Beanstalk\Adapter::class,
             \Fusio\Adapter\Cli\Adapter::class,
             \Fusio\Adapter\Fcgi\Adapter::class,
             \Fusio\Adapter\File\Adapter::class,
             \Fusio\Adapter\GraphQL\Adapter::class,
             \Fusio\Adapter\Http\Adapter::class,
             \Fusio\Adapter\Php\Adapter::class,
+            \Fusio\Adapter\Redis\Adapter::class,
             \Fusio\Adapter\Smtp\Adapter::class,
             \Fusio\Adapter\Soap\Adapter::class,
             \Fusio\Adapter\Sql\Adapter::class,
