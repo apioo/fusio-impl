7646bcda217c289f4c1dc843b8d002504c7efd2a
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-06-08 23:58:44 +0200

    fix test case

--- tests/Backend/Api/Database/Table/EntityTest.php
+++ tests/Backend/Api/Database/Table/EntityTest.php
@@ -46,7 +46,7 @@ class EntityTest extends ControllerDbTestCase
             'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
         ));
 
-        if ($this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+        if ($this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform && PHP_MAJOR_VERSION === 8 && PHP_MINOR_VERSION === 2) {
             $length = 0;
         } else {
             $length = 'null';
