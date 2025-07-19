b57b33450c33921e5c72cafe455396c004c4803c
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-07-19 20:48:02 +0200

    Revert "remove legacy schema form"
    This reverts commit eaeafb11e4238b5962b32d0816832123d67dbb3b.

---
+++ src/Backend/Action/Schema/Form.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Fusio - Self-Hosted API Management for Builders.
+ * For the current version and information visit <https://www.fusio-project.org/>
+ *
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Fusio\Impl\Backend\Action\Schema;
+
+use Fusio\Engine\ActionInterface;
+use Fusio\Engine\ContextInterface;
+use Fusio\Engine\ParametersInterface;
+use Fusio\Engine\RequestInterface;
+use Fusio\Impl\Service\Schema;
+use Fusio\Impl\Service\System\ContextFactory;
+use Fusio\Model\Backend\SchemaForm;
+
+/**
+ * GetForm
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    https://www.fusio-project.org
+ */
+class Form implements ActionInterface
+{
+    private Schema $schemaService;
+    private ContextFactory $contextFactory;
+
+    public function __construct(Schema $schemaService, ContextFactory $contextFactory)
+    {
+        $this->schemaService = $schemaService;
+        $this->contextFactory = $contextFactory;
+    }
+
+    public function handle(RequestInterface $request, ParametersInterface $configuration, ContextInterface $context): mixed
+    {
+        $body = $request->getPayload();
+
+        assert($body instanceof SchemaForm);
+
+        $this->schemaService->updateForm(
+            $request->get('schema_id'),
+            $body,
+            $this->contextFactory->newActionContext($context)
+        );
+
+        return array(
+            'success' => true,
+            'message' => 'Schema form successfully updated',
+        );
+    }
+}
