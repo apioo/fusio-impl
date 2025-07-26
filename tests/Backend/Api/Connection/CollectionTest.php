236bddaea2662022217fd299f568c40ac6bbdd7a
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-07-26 22:11:23 +0200

    fix test for postgres

--- tests/Backend/Api/Connection/CollectionTest.php
+++ tests/Backend/Api/Connection/CollectionTest.php
@@ -91,7 +91,7 @@ JSON;
 
     public function testGetSearch()
     {
-        $response = $this->sendRequest('/backend/connection?search=Sys', 'GET', array(
+        $response = $this->sendRequest('/backend/connection?search=yst', 'GET', array(
             'User-Agent'    => 'Fusio TestCase',
             'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
         ));
