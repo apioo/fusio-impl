dd894ea16e1d2ce7a7b41aa26e274d9cc993b22c
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-10-09 00:15:05 +0200

    add trigger test

---
+++ tests/Backend/Api/Trigger/EntityTest.php
@@ -0,0 +1,189 @@
+<?php
+/*
+ * Fusio - Self-Hosted API Management for Builders.
+ * For the current version and information visit <https://www.fusio-project.org/>
+ *
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Fusio\Impl\Tests\Backend\Api\Trigger;
+
+use Fusio\Impl\Table;
+use Fusio\Impl\Tests\DbTestCase;
+
+/**
+ * EntityTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    https://www.fusio-project.org
+ */
+class EntityTest extends DbTestCase
+{
+    public function testGet()
+    {
+        $response = $this->sendRequest('/backend/trigger/1', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<JSON
+{
+    "id": 1,
+    "status": 1,
+    "name": "Test-Trigger",
+    "event": "foo-event",
+    "action": "Sql-Select-All",
+    "metadata": {
+        "foo": "bar"
+    }
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testGetByName()
+    {
+        $response = $this->sendRequest('/backend/trigger/~Test-Trigger', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<JSON
+{
+    "id": 1,
+    "status": 1,
+    "name": "Test-Trigger",
+    "event": "foo-event",
+    "action": "Sql-Select-All",
+    "metadata": {
+        "foo": "bar"
+    }
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+    }
+
+    public function testGetNotFound()
+    {
+        $response = $this->sendRequest('/backend/trigger/10', 'GET', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body = (string) $response->getBody();
+        $data = \json_decode($body);
+
+        $this->assertEquals(404, $response->getStatusCode(), $body);
+        $this->assertFalse($data->success);
+        $this->assertStringStartsWith('Could not find trigger', $data->message);
+    }
+
+    public function testPost()
+    {
+        $response = $this->sendRequest('/backend/trigger/1', 'POST', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ), json_encode([
+            'foo' => 'bar',
+        ]));
+
+        $body = (string) $response->getBody();
+
+        $this->assertEquals(404, $response->getStatusCode(), $body);
+    }
+
+    public function testPut()
+    {
+        $metadata = [
+            'foo' => 'bar'
+        ];
+
+        $response = $this->sendRequest('/backend/trigger/1', 'PUT', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ), json_encode([
+            'name'     => 'Foo-Trigger',
+            'event'    => 'foo-event',
+            'action'   => 'Inspect',
+            'metadata' => $metadata,
+        ]));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": true,
+    "message": "Trigger successfully updated",
+    "id": "1"
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+
+        // check database
+        $sql = $this->connection->createQueryBuilder()
+            ->select('id', 'name', 'event', 'action', 'metadata')
+            ->from('fusio_trigger')
+            ->where('id = :id')
+            ->getSQL();
+
+        $row = $this->connection->fetchAssociative($sql, ['id' => 1]);
+
+        $this->assertEquals(1, $row['id']);
+        $this->assertEquals('Foo-Trigger', $row['name']);
+        $this->assertEquals('foo-event', $row['event']);
+        $this->assertEquals('Inspect', $row['action']);
+        $this->assertJsonStringEqualsJsonString(json_encode($metadata), $row['metadata']);
+    }
+
+    public function testDelete()
+    {
+        $response = $this->sendRequest('/backend/trigger/1', 'DELETE', array(
+            'User-Agent'    => 'Fusio TestCase',
+            'Authorization' => 'Bearer da250526d583edabca8ac2f99e37ee39aa02a3c076c0edc6929095e20ca18dcf'
+        ));
+
+        $body   = (string) $response->getBody();
+        $expect = <<<'JSON'
+{
+    "success": true,
+    "message": "Trigger successfully deleted",
+    "id": "1"
+}
+JSON;
+
+        $this->assertEquals(200, $response->getStatusCode(), $body);
+        $this->assertJsonStringEqualsJsonString($expect, $body, $body);
+
+        // check database
+        $sql = $this->connection->createQueryBuilder()
+            ->select('id', 'status')
+            ->from('fusio_trigger')
+            ->where('id = :id')
+            ->getSQL();
+
+        $row = $this->connection->fetchAssociative($sql, ['id' => 1]);
+
+        $this->assertEquals(1, $row['id']);
+        $this->assertEquals(Table\Trigger::STATUS_DELETED, $row['status']);
+    }
+}
