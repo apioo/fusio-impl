dce1cbf31d89528f3b3ba5b65f8b504dbd6e17ac
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-02-14 17:37:55 +0100

    fix test

--- tests/Backend/Api/Operation/CollectionTest.php
+++ tests/Backend/Api/Operation/CollectionTest.php
@@ -584,7 +584,7 @@ JSON;
         $this->assertEquals(201, $response->getStatusCode(), $body);
         $this->assertSame(true, $data->success);
         $this->assertSame('Operation successfully created', $data->message);
-        $this->assertContains($data->id, ['276', '277']); // postgres does not reset the auto increment so we need to check both
+        $this->assertContains($data->id, ['277', '278']); // postgres does not reset the auto increment so we need to check both
 
         // check database
         Assert::assertOperation($this->connection, OperationInterface::STABILITY_EXPERIMENTAL, 'test.bar', 'GET', '/foo/bar', 200, ['foo', 'baz'], $metadata);
