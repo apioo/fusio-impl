bf2ace3e54bb93d5fad05ce70f4e13b21c976327
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-05-01 09:27:19 +0200

    add return types

--- tests/resources/local-php.php
+++ tests/resources/local-php.php
@@ -3,7 +3,8 @@
 use Fusio\Worker;
 use Fusio\Engine;
 use Psr\Log\LoggerInterface;
+use PSX\Http\Environment\HttpResponseInterface;
 
-return fn(Worker\ExecuteRequest $request, Worker\ExecuteContext $context, Engine\ConnectorInterface $connector, Engine\Response\FactoryInterface $response, Engine\DispatcherInterface $dispatcher, LoggerInterface $logger) => $response->ok([
+return fn(Worker\ExecuteRequest $request, Worker\ExecuteContext $context, Engine\ConnectorInterface $connector, Engine\Response\FactoryInterface $response, Engine\DispatcherInterface $dispatcher, LoggerInterface $logger): HttpResponseInterface => $response->ok([
     'foo' => 'bar',
 ]);
