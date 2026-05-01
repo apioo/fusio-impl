a198c47d390d7f7b10f35f2b8dfd839801b7a199
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-05-01 09:58:39 +0200

    add rector config

---
+++ rector.php
@@ -0,0 +1,33 @@
+<?php
+
+use Rector\CodeQuality\Rector\BooleanOr\RepeatedOrEqualToInArrayRector;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\SafeDeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets()
+    ->withComposerBased(phpunit: true)
+    ->withSets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        SetList::PRIVATIZATION,
+        SetList::TYPE_DECLARATION,
+    ])
+    ->withSkip([
+        SimplifyUselessVariableRector::class,
+        RemoveUnusedVariableAssignRector::class,
+        SafeDeclareStrictTypesRector::class,
+        RepeatedOrEqualToInArrayRector::class,
+        DisallowedEmptyRuleFixerRector::class,
+        CatchExceptionNameMatchingTypeRector::class,
+    ]);
